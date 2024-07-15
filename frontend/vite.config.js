3c38d59bf3d7ec6d5096b0d68275ff906e7f8d82
César <cesarlpb@gmail.com>
2024-07-15 19:23:35 +0200

    proyecto de React para hacer build

---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
+})
